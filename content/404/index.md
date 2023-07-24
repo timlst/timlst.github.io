bff8e3950c21362b23e023278d40a39b7613b098
Tim Storm <tim.storm@cispa.de>
2023-07-24 18:55:16 +0200

    dont know how but now posts do display

--- content/404/index.md
+++
@@ -1,4 +0,0 @@
-# 404
-“Fate lies within the light cone.”
-― Liu Cixin, The Dark Forest
-
--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,7 @@ USER=tistorm
 HOST=timlst.de
 DIR=personal-website/   # the directory where your web site files should go
 
-hugo && rsync -avz --delete public/ ${USER}@${HOST}:~/${DIR} # this will delete everything on the server that's not in the local public folder
+hugo&& rsync -avz --delete public/ ${USER}@${HOST}:~/${DIR} # this will delete everything on the server that's not in the local public folder
 
 exit 0
 
